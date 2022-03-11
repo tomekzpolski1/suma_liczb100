8c8309463228f56a99d72cfe8d36f23cfd708860
tomekzpolski1 <tomasz.rozynski92@gmail.com>
2022-03-11 19:21:51 +0100

    Add project files.

---
+++ ConsoleApp26/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+            int i = 0;
+            while (suma <= 100)
+            {
+                suma += i;
+                if (suma < 100)
+                {
+                    i++;
+                }
+
+            }
+            Console.WriteLine("Trzeba dodać do siebie {0} liczb", i);
+        }
+    }
+}
